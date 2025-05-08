af90a4bb730b8b5730730fa1fb707cdb6402a391
Joseph Huamani | Nerk <josephhuamanilol@gmail.com>
2025-05-07 21:13:22 -0500

    feat: added donation list page
    I've added donation list

---
+++ src/app/public/donations/pages/donation-list/donation-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-list',
+  imports: [],
+  templateUrl: './donation-list.component.html',
+  styleUrl: './donation-list.component.css'
+})
+export class DonationListComponent {
+
+}
