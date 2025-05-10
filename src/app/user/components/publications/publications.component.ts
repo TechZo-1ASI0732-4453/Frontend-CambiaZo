88d46e5bc01f0a3e0f1e2c3c158051956b975ee6
Joseph Huamani | Nerk <112527806+HelloNerk@users.noreply.github.com>
2025-05-09 20:21:29 -0500

    fix: publications

--- src/app/user/components/publications/publications.component.ts
+++ src/app/user/components/publications/publications.component.ts
@@ -1,20 +1,25 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, input, InputSignal, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
+import { Product } from '../../../public/home/models/product.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-publications',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './publications.component.html',
   styleUrl: './publications.component.css'
 })
-export class PublicationsComponent implements OnInit {
-  
+export class PublicationsComponent {
+
   private readonly userService: UserService=inject(UserService);
 
-  ngOnInit(): void {
-    this.userService.getProductByUserId(1).subscribe((products) => {
-      console.log(products);
-    });
+  userId: InputSignal<number> = input.required();
+
+  products$: Observable<Product[]>=this.userService.getProductByUserId(this.userId());;
+
+  constructor() {
+    console.log(this.userId());
   }
-  
+
 }
--- src/app/user/pages/profile-page/profile-page.component.ts
+++ src/app/user/pages/profile-page/profile-page.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { ProfileSidebarComponent } from '../../components/profile-sidebar/profile-sidebar.component';
 import { SignInUser } from '../../../auth/models/signInUser.model';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { AsyncPipe } from '@angular/common';
+import { PublicationsComponent } from "../../components/publications/publications.component";
 
 @Component({
   selector: 'app-profile-page',
-  imports: [ProfileSidebarComponent, AsyncPipe],
+  imports: [ProfileSidebarComponent, AsyncPipe, PublicationsComponent],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
   
   user$: Observable<SignInUser>;
-  selectedIndex: number = 0;
+  selectedIndex: WritableSignal<number> = signal(0);
 
   constructor(private route: ActivatedRoute, private userService: UserService) {
     this.user$ = this.route.params.pipe(
@@ -24,7 +25,7 @@ export class ProfilePageComponent {
   }
 
   receiveSelectedIndex(index: number) {
-    this.selectedIndex = index;
+    this.selectedIndex.set(index);
   }
 
 }
